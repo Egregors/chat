290a6f6090c0fc73d5f4950a441ae63091c6d53d
Vadim Iskuchekov <root@egregors.com>
2018-02-23 17:14:13 +1000

    Connect handler

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net"
 	"log"
+	"bufio"
+	"fmt"
 )
 
 type client chan<- string
@@ -31,19 +33,44 @@ func broadcaster() {
 	}
 }
 
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outcome messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You " + who
+	messages <- who + " connected"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	leaving <- ch
+	messages <- who + " disconnected"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
+
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//go broadcaster()
+	go broadcaster()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
-		//go handleConn(conn)
+		go handleConn(conn)
 	}
 }
