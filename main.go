730ea731f9aeb005814f326f5b1bdd816b09adab
Vadim Iskuchekov <root@egregors.com>
2018-02-23 17:07:48 +1000

    Broadcaster

--- main.go
+++ main.go
@@ -5,6 +5,32 @@ import (
 	"log"
 )
 
+type client chan<- string
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all inbox messages
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// broadcast income message for all outcome messages for clients
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8000")
 	if err != nil {
